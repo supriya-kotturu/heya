b3293885661eaed001a1fdb59e43d9de7399d619
supriya-kotturu <supriyakotturu@pm.me>
2021-08-13 02:44:10 +0530

    style : tailwind palette colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import Card from "./components/Card";
 function App() {
   return (
-    <div className="App">
+    <div className="App p-3">
       <header className="App-header">
         <div>
-          <p className="font-sans font-extralight text-2xl">Heya!</p>
-          <span className="font-sans  font-semibold">Contacts. Made easy.</span>
+          <p className="font-sans  text-blue-002 font-extrabold text-2xl">
+            Heya!
+          </p>
+          <span className="font-sans text-medium-01 ">
+            Contacts. Made easy.
+          </span>
         </div>
       </header>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-white-01 border-2 border-blue-002  p-6 rounded-lg m-6">
+      <p>
+        <h1 className="text-3xl font-light ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+          neque?
+        </h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        distinctio molestias quidem neque architecto? Ab neque sed amet soluta
+        libero dolores mollitia deserunt animi quaerat, quod modi temporibus
+        nulla quos.
+      </p>
+      <p>
+        NAME OJNE <br /> 78136679
+      </p>
+      <div className=" p-3 text-medium-02 rounded-lg  m-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum id,
+        laborum dolores quam fuga obcaecati? Vero incidunt quia corrupti hic.
+      </div>
+    </div>
+  );
+};
+
+export default Card;
